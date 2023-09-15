f275b8d7b020e12e569f3aabb65c2a236af4a765
chuxing <3098694931@qq.com>
2023-09-15 21:19:26 +0800

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,25 @@
+/**
+ * ClassName:  HelloWorld
+ * Description:
+ *
+ * @author: 储兴
+ * @Crate 2023/9/15 21:18
+ * @version: 1.0
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("helloWorld222\n" +
+                "helloWorld333\n" +
+                "helloWorld\n" +
+                "helloWorld\n" +
+                "helloWorld\n" +
+                "helloWorld\n" +
+                "helloWorld\n" +
+                "helloWorld\n" +
+                "helloWorld\n" +
+                "helloWorld\n" +
+                "helloWorld\n" +
+                "helloWorld test\n" +
+                "helloWorld test1");
+    }
+}
